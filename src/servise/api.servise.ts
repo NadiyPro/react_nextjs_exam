a83fdb98542c30e842258d3629028aaf153e2d8a
NadiyPro <siroviyn13@gmail.com>
2024-08-12 21:27:44 +0300

    create

--- src/servise/api.servise.ts
+++ src/servise/api.servise.ts
@@ -1,8 +1,14 @@
+import {IMovie} from "@/models/IMovie";
+
 const base:string = 'https://api.themoviedb.org/3'; // базова для всіх окрім малюнків
 const baseImg:string = 'https://image.tmdb.org/t/p/w500'; // базова для малюнків
 
-const movieService = async () => {
+const movieService = async ():Promise<IMovie> => {
     let [movie] = await Promise.all([fetch(base + '/discover/movie')
         .then(response => response.json())])
     return movie;
+}
+
+export {
+    movieService
 }
