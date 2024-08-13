9c95d18f7dc81d94c4a7f0f7c990d61c250ed548
NadiyPro <siroviyn13@gmail.com>
2024-08-13 20:45:39 +0300

    create

--- src/app/(way)/discover/movie/page.tsx
+++ src/app/(way)/discover/movie/page.tsx
@@ -4,6 +4,7 @@ import {baseImg, movieService} from "@/servise/api.servise";
 import {IMovie} from "@/models/IMovie";
 import {useSearchParams} from "next/navigation";
 import PaginationComponent from "@/components/PaginationComponent";
+import style from '@/moduleCSS/style.module.css'
 
 
 
@@ -12,13 +13,17 @@ const AllMoviesLayout =  async () => {
     const currentPage = +(searchParams.get('page') || 1);
     const movies = await movieService(currentPage) as IMovie[];
     return (
-        <div>
-            {
-                movies.map(value => <div key={value.id}>
-                    <img src={baseImg + value.poster_path} alt={'image'}/>
-                    <p>{value.id} {value.title}</p></div> )
-            }
-            <PaginationComponent currentPage={currentPage} />
+        <div className={style.div_img_title_next}>
+            <div className={style.div_title}>
+                {
+                    movies.map(value => <div key={value.id} className={style.div_img_title}>
+                        <img className={style.img_title} src={baseImg + value.poster_path} alt={'image'} />
+                        <p>{value.title}</p></div> )
+                }
+            </div>
+           <div>
+               <PaginationComponent currentPage={currentPage} />
+           </div>
         </div>
     );
 };
--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,29 +1,45 @@
 'use client';
 
 import React, {useEffect, useState} from 'react';
-import {useSearchParams} from "next/navigation";
+import {usePathname, useSearchParams} from "next/navigation";
 import {movieService} from "@/servise/api.servise";
 import {IPageMovie} from "@/models/IPageMovie";
 
 const PaginationComponent = ({currentPage:number}) => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [moviePage, setMoviePage] = useState<IPageMovie[] > ([]);
+    const searchParams = useSearchParams(); // используем useSearchParams для работы с параметрами URL
+    const pathname = usePathname(); // используем usePathname для получения текущего пути
+    const [moviePage, setMoviePage] = useState<IPageMovie[] | null>(null);
+
     useEffect(() => {
-        const currentPage = +(searchParams.get('page')!) || 1;
+        const currentPage = +(searchParams.get('page')) || 1;
         movieService(currentPage).then(value => {
-            setMoviePage(value)
-            return currentPage;
-        })
+            setMoviePage(value);
+        });
     }, [searchParams]);
+
+    const handlePageChange = (page: number) => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set('page', page.toString());
+        window.history.pushState({}, '', `${pathname}?${params.toString()}`);
+    };
+
+    // const [searchParams, setSearchParams] = useSearchParams();
+    // const [moviePage, setMoviePage] = useState<IPageMovie[] > ([]);
+    // useEffect(() => {
+    //     const currentPage = +(searchParams.get('page')!) || 1;
+    //     movieService(currentPage).then(value => {
+    //         setMoviePage(value)
+    //         return currentPage;
+    //     })
+    // }, [searchParams]);
 
 
     return (
         <div>
             <button onClick={() => {
                 const page = +(searchParams.get('page')) || 1;
-                if (page) {
-                    setSearchParams({ page: (page + 1).toString() });
-            }}}>Next
+                handlePageChange(page + 1);
+            }}>Next
             </button>
 
         </div>
