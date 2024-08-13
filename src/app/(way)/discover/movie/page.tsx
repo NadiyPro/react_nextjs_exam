f9e579e2b58238efeabb901b333237682d44620e
NadiyPro <siroviyn13@gmail.com>
2024-08-13 17:39:12 +0300

    create

--- src/app/(way)/discover/movie/page.tsx
+++ src/app/(way)/discover/movie/page.tsx
@@ -1,18 +1,21 @@
+'use client';
 import React from 'react';
-
-import PagComponent from "@/components/PagComponent";
 import {movieService} from "@/servise/api.servise";
 import {IMovie} from "@/models/IMovie";
+import PaginationComponent from "@/components/PaginationComponent";
+import {useSearchParams} from "next/navigation";
 
 
-const AllMoviesLayout =  async ({page:number}) => {
-    let movies= await movieService(1) as IMovie[];
+const AllMoviesLayout =  async () => {
+    const searchParams = useSearchParams();
+    const currentPage = +(searchParams.get('page') || 1);
+    const movies = await movieService(currentPage) as IMovie[];
     return (
         <div>
             {
                 movies.map(value => <div key={value.id}>{value.id} {value.title}</div>)
             }
-            <PagComponent/>
+            <PaginationComponent currentPage={currentPage} />
         </div>
     );
 };
--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,34 +1,35 @@
-// 'use client';
-//
-// import React, {useEffect, useState} from 'react';
-// import {useSearchParams} from "next/navigation";
-// import {movieService} from "@/servise/api.servise";
-// import {IPageMovie} from "@/models/IPageMovie";
-//
-// const PaginationComponent = ({page:number}) => {
-//     const [query, setQuery] = useSearchParams();
-//     const [moviePage, setMoviePage] = useState<IPageMovie[]> ([]);
-//     useEffect(() => {
-//         const currentPage = +(query.get('page')) || 1;
-//         movieService(currentPage).then(value => {
-//             setMoviePage(value)
-//             console.log(value)
-//         })
-//     }, [query]);
-//     return (
-//         <div>
-//             <button onClick={() => {
-//                 const page = +(query.get('page')) || 1;
-//                 if (page) {
-//                     let currentPage = +page;
-//                     currentPage++;
-//                     setQuery({page: currentPage})
-//                 }
-//             }}>Next
-//             </button>
-//
-//         </div>
-//     );
-// };
+'use client';
 
-// export default PaginationComponent;
+import React, {useEffect, useState} from 'react';
+import {useSearchParams} from "next/navigation";
+import {movieService} from "@/servise/api.servise";
+import {IPageMovie} from "@/models/IPageMovie";
+import {IMovie} from "@/models/IMovie";
+
+const PaginationComponent = ({currentPage:number}) => {
+    const [query, setQuery] = useSearchParams();
+    const [moviePage, setMoviePage] = useState<IPageMovie[]> ([]);
+    useEffect(() => {
+        const currentPage = +(query.get('page')) || 1;
+        movieService(currentPage).then(value => {
+            setMoviePage(value)
+            console.log(value)
+        })
+    }, [query]);
+    return (
+        <div>
+            <button onClick={() => {
+                const page = +(query.get('page')) || 1;
+                if (page) {
+                    let currentPage = +page;
+                    currentPage++;
+                    setQuery({page: currentPage})
+                }
+            }}>Next
+            </button>
+
+        </div>
+    );
+};
+
+export default PaginationComponent;
