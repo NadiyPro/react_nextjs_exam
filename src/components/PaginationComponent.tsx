107c323d572a544578b9a479f03c430438aa9525
NadiyPro <siroviyn13@gmail.com>
2024-08-14 07:50:09 +0300

    create

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -1,7 +1,35 @@
+// 'use client';
+// import React from "react";
+//
+// const PaginationComponent = ({currentPage}) => {
+//
+//     return (
+//         <div>
+//             <button
+//                 onClick={() => {
+//                     currentPage++;}
+//                 }
+//             >
+//                 next
+//             </button>
+//             {currentPage}
+//             <button
+//                 onClick={() => {
+//                     currentPage--;}
+//                 }
+//             >
+//                 prev
+//             </button>
+//         </div>
+//     );
+// };
+//
+// export default PaginationComponent;
 'use client';
 import React from "react";
 
 const PaginationComponent = ({currentPage}) => {
+
     return (
         <div>
             <button
