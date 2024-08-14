3edb058c406d20453eace5b784049882e6a9c01a
NadiyPro <siroviyn13@gmail.com>
2024-08-14 14:12:41 +0300

    create

--- src/app/(way)/genre/movie/list/page.tsx
+++ src/app/(way)/genre/movie/list/page.tsx
@@ -2,18 +2,30 @@ import {getGenres} from "@/servise/api.servise";
 import {IMovieList} from "@/models/IMovieList";
 import React from "react";
 import style from "@/moduleCSS/style.module.css";
+import Link from "next/link";
 
 const MoviesList = async () => {
     const movieList = await getGenres() as IMovieList[];
     console.log(movieList)
     return (
+        // <div className={style.div_MoviesList}>
+        //     {movieList.map(value =>
+        //         <div key={value.id}>
+        //             <p className={style.p_MoviesList}>{value.name}</p>
+        //         </div>
+        //     )}
+        // </div>
         <div className={style.div_MoviesList}>
-            {movieList.map(value =>
-                <div key={value.id}>
-                    <p className={style.p_MoviesList}>{value.name}</p>
-                </div>
-            )}
+            <div>
+                {movieList.map(value =>
+                    <div key={value.id} className={style.p_MoviesList}>
+                        <Link href={`/movie/${value.id}/changes`}>{value.name}</Link>
+                    </div>
+                )}
+            </div>
         </div>
+
+
     );
 };
 
