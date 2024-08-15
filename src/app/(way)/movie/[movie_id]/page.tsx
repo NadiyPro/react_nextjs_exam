af3be99a1bfce959fb53e8db2e6890b5183c2f2d
NadiyPro <siroviyn13@gmail.com>
2024-08-15 20:08:35 +0300

    create

--- src/app/(way)/movie/[movie_id]/page.tsx
+++ src/app/(way)/movie/[movie_id]/page.tsx
@@ -7,11 +7,12 @@ import Link from "next/link";
 const MovieInfo = async ({ params }: { params: { movie_id: string } }) => {
     let movie_id:number = +params.movie_id;
     let movie_info:IMovieInfo = await getMovieInfo(movie_id);
-    let genres_id = movie_info.genres.map(value => value.id) as number;
     let genres_name = movie_info.genres.map(value => <Link key={value.id} href={{
-        pathname: '/discover/movie',
-        query: {with_genres: value.id, page: 1}
-    }}><h5>{value.name}</h5></Link>) as string;
+                             pathname: '/discover/movie', query: {with_genres: value.id, page: 1}}}>
+                             <h5>{value.name}</h5></Link>) as string;
+    let production_countries = movie_info.production_countries.map(value => value.name) as string;
+    let spoken_languages = movie_info.spoken_languages.map(value => value.name) as string;
+
     return (
         <div>
             <div>
@@ -26,18 +27,12 @@ const MovieInfo = async ({ params }: { params: { movie_id: string } }) => {
 
                 <div>
                     <div><h2>{movie_info.title}</h2></div>
-                    <div>
-                        <h5>{genres_name}</h5>
-                        {/*<Link href={{*/}
-                        {/*    pathname: '/discover/movie',*/}
-                        {/*    query: {with_genres: genres_id, page: 1}*/}
-                        {/*}}>{genres_name}</Link>*/}
-                    </div>
-                    <div><h4>release date: {movie_info.release_date}</h4></div>
-                    {/*<div><h6>production countries: {movie_info.production_countries}</h6></div>*/}
-                    <div><h4>original language: {movie_info.original_language}</h4></div>
-                    {/*<div><h6>spoken languages: {movie_info.spoken_languages}</h6></div>*/}
-                    <div><h4>vote count: {movie_info.vote_count} vote average: {movie_info.vote_average}</h4></div>
+                    <div><h5>Genres: {genres_name}</h5></div>
+                    <div><h5>Release date: {movie_info.release_date}</h5></div>
+                    <div><h5>Production countries: {production_countries}</h5></div>
+                    <div><h5>Original language: {movie_info.original_language}</h5></div>
+                    <div><h5>Spoken languages: {spoken_languages}</h5></div>
+                    <div><h5>Vote count: {movie_info.vote_count} / Vote average: {movie_info.vote_average}</h5></div>
                 </div>
             </div>
 
