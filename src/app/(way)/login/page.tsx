57c9567822485f996061935fc9bc7d3f29628e54
NadiyPro <siroviyn13@gmail.com>
2024-08-17 22:55:01 +0300

    create

--- src/app/(way)/login/page.tsx
+++ src/app/(way)/login/page.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import Link from "next/link";
+import FormComponent from "@/component/FormComponent";
+import style from "@/moduleCSS/style.module.css";
 
 const LoginPage = () => {
     return (
-        <div>
-            <form method={'GET'} action={'/search/movie'}>
-                <label form="name">Enter your login</label>
-                <input type={"text"} name={'name'} placeholder={'Nadia Prosianyk'}
-                       required={true}/><sup>*</sup>
+        <div className={style.div_LoginPage_LoginPage}>
+            <FormComponent/>
+            <div className={style.div_LoginPage}>
+                <div>
+                    <img className={style.img_LoginPage} src={'https://www.pngall.com/wp-content/uploads/14/Welcome-Background-PNG.png'} alt={'img'}/>
+                </div>
+                <div>
+                    <form className={style.form_LoginPage} method={'GET'} action={'/search/movie'}>
+                        <label form="name">Enter your login</label>
+                        <input type={"text"} name={'name'} placeholder={'Nadia Prosianyk'}
+                               required={true}/>
 
-                <label form="password">Enter your password</label>
-                <input type={"password"} name={'password'} placeholder={'*****'}
-                       required={true}/><sup>*</sup>
-                <button><Link href={'/discover/movie?page=1'}>Search</Link></button>
-            </form>
+                        <label form="password">Enter your password</label>
+                        <input type={"password"} name={'password'} placeholder={'**********'}
+                               required={true}/>
+                        <div>
+                            <button className={style.button_pagination}>
+                                <Link href={'/discover/movie?page=1'} className={style.p_MoviesPage}>
+                                    Login
+                                </Link>
+                            </button>
+                            <button className={style.button_pagination}>
+                                <Link href={'/discover/movie?page=1'} className={style.p_MoviesPage}>
+                                    Home
+                                </Link>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
