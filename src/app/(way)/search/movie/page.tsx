9f101239204cea0d0ce1a048bb6b06cb13bc75b3
NadiyPro <siroviyn13@gmail.com>
2024-08-17 18:18:21 +0300

    create

--- src/app/(way)/search/movie/page.tsx
+++ src/app/(way)/search/movie/page.tsx
@@ -17,20 +17,20 @@ const MovieSearchPage = async ({searchParams: {query,page}}:ISearchProps) => {
 console.log(search)
 
     return (
-        <div className={style.div_MoviesPage}>
+        <div className={style.div_MovieSearchPage}>
             <FormComponent/>
-            <div className={style.div_inner_MoviesPage}>
+            <div className={style.div_inner_MovieSearchPage}>
                 {Array.isArray(search) ?
                     search.map(value =>
-                        <div key={value.id} className={style.div_img_title}>
+                        <div key={value.id} className={style.div_img_title_MovieSearchPage}>
                             <div key={value.id} >
                                 {/* eslint-disable-next-line @next/next/no-img-element */}
                                 <Link href={`/movie/${value.id}`}><img className={style.img_title}
                                                                        src={baseImg + value.poster_path}
                                                                        alt={'image'}/></Link>
                                 <div>
-                                    <Link href={`/movie/${value.id}`} className={style.Link_MoviesPage}>
-                                        <p className={style.p_MoviesPage}>{value.title}</p>
+                                    <Link href={`/movie/${value.id}`} className={style.div_Lint_title_MovieSearchPage}>
+                                        <p className={style.p_MovieSearchPage}>{value.title}</p>
                                     </Link>
                                 </div>
                             </div>
@@ -42,18 +42,18 @@ console.log(search)
                     <div><p>There are no search results for your request</p></div>
                 }
             </div>
-            <div className={style.div_button_MoviesPage}>
-                <button className={style.button_pagination}>
-                    <Link href={`/search/movie?query=${query}&page=${pageSearch > 1 ? pageSearch - 1 : 1}`} className={style.p_MoviesPage}>
+            <div className={style.div_button_MovieSearchPage}>
+                <button  className={style.button_pagination_MovieSearchPage}>
+                    <Link href={`/search/movie?query=${query}&page=${pageSearch > 1 ? pageSearch - 1 : 1}`} className={style.p_MovieSearchPage}>
                         Prev {pageSearch > 1 ? pageSearch - 1 : 1}</Link>
                 </button>
                 <h2>{page}</h2>
-                <button className={style.button_pagination}>
-                <Link href={`/search/movie?query=${query}&page=${pageSearch < 1 ? 1 : +pageSearch + 1}`} className={style.p_MoviesPage}>
+                <button className={style.button_pagination_MovieSearchPage}>
+                <Link href={`/search/movie?query=${query}&page=${pageSearch < 1 ? 1 : +pageSearch + 1}`} className={style.p_MovieSearchPage}>
                         Next {pageSearch < 1 ? 1 : +pageSearch + 1}</Link>
                 </button>
-                <button className={style.button_pagination}>
-                    <Link href={'/discover/movie?page=1'} className={style.p_MoviesPage}>
+                <button className={style.button_pagination_MovieSearchPage} >
+                    <Link href={'/discover/movie?page=1'} className={style.p_MovieSearchPage}>
                         Home
                     </Link>
                 </button>
